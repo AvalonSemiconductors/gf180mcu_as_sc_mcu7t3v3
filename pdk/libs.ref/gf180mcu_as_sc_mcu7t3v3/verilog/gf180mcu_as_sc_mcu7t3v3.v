19031a2357c2d14e63b609aa6b387f8d1200ab4b
Egor Lukyanchenko <13577050+egorxe@users.noreply.github.com>
2026-05-27 21:18:15 +0500

    Add several missing Verilog models and fix gf180mcu_as_sc_mcu7t3v3__dfsrtp_2

--- pdk/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
+++ pdk/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
@@ -49,16 +49,17 @@ reg state;
 wire sr;
 
 assign sr = ~(RN & SN);
+assign Q = state;
 
 always @(posedge CLK or posedge sr) begin
 	if (sr == 1'b1) begin
 	if (RN == 1'b0) begin
-		Q <= 1'b0;
+		state <= 1'b0;
 	end else if (SN == 1'b0) begin
-		Q <= 1'b1;
+		state <= 1'b1;
 	end
 	end else begin
-		Q <= state;
+		state <= D;
 	end
 end
 
@@ -598,6 +599,40 @@ assign Y = ~((A & B) | C);
 
 endmodule
 
+module gf180mcu_as_sc_mcu7t3v3__aoi211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A & B) | C | D);
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__aoi211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A & B) | C | D);
+
+endmodule
+
 module gf180mcu_as_sc_mcu7t3v3__aoi21_4 (
 	input VPW,
 	input VNW,
@@ -659,6 +694,40 @@ module gf180mcu_as_sc_mcu7t3v3__ao21_2 (
 );
 
 assign Y = (A & B) | C;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = (A & B) | C | D;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = (A & B) | C | D;
 
 endmodule
 
@@ -843,6 +912,24 @@ module gf180mcu_as_sc_mcu7t3v3__ao31_4 (
 );
 
 assign Y = (A & B & C) | D;
+
+endmodule
+
+
+module gf180mcu_as_sc_mcu7t3v3__oai211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~((A | B) & C & D);
 
 endmodule
 
