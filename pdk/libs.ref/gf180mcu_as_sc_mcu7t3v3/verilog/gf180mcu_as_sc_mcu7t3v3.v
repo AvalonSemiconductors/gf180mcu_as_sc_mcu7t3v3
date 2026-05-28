40c04753c43752cf702d3f7c6e69cd37c7d6897e
Tholin <cutie@tholin.dev>
2026-05-28 02:31:15 +0200

    Fix BROKEN lib files! Add missing verilog models.

--- pdk/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
+++ pdk/libs.ref/gf180mcu_as_sc_mcu7t3v3/verilog/gf180mcu_as_sc_mcu7t3v3.v
@@ -812,6 +812,74 @@ assign Y = ~((A & B & C) | D);
 
 endmodule
 
+module gf180mcu_as_sc_mcu7t3v3__aoi211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~(C | D | (A & B));
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__aoi211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~(C | D | (A & B));
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = C | D | (A & B);
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__ao211_4 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = C | D | (A & B);
+
+endmodule
+
 module gf180mcu_as_sc_mcu7t3v3__ao31_2 (
 	input VPW,
 	input VNW,
@@ -843,6 +911,23 @@ module gf180mcu_as_sc_mcu7t3v3__ao31_4 (
 );
 
 assign Y = (A & B & C) | D;
+
+endmodule
+
+module gf180mcu_as_sc_mcu7t3v3__oai211_2 (
+	input VPW,
+	input VNW,
+	input VDD,
+	input VSS,
+
+	input A,
+	input B,
+	input C,
+	input D,
+	output Y
+);
+
+assign Y = ~(C & D & (A | B));
 
 endmodule
 
